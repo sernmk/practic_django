d6f1a7ae018983fd492ead2835be89c5fb8bc9a4
sernmk <sernmk@gmail.com>
2018-01-22 21:55:42 +0300

    Modif

--- blog/models.py
+++ blog/models.py
@@ -6,7 +6,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
-    publish_date = models.DateTimeField(blank=True, null = True)
+    published_date = models.DateTimeField(blank=True, null = True)
 
     def publish(self):
         self.publish_date = timezone.now()
